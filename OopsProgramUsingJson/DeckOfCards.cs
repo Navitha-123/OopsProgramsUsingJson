ed321d7f06768de4147cc9af5013f8050b6c5a2e
navitha <navithareddy112@gmail.com>
2023-01-08 20:16:01 +0530

    [navitha] UC1_FetchForJsonRiceData

---
+++ OopsProgramUsingJson/DeckOfCards.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsProgramUsingJson
+{
+    internal class DeckOfCards
+    {
+        public static void IntializeDeckOfCards()
+        {
+            DeckOfCards deckOfCards = new DeckOfCards();
+            String[] suits =
+            {
+                "clubs","Diamonds","Hearts", "Spades"
+            };
+            String[] Ranks =
+            {
+                "2","3","4","5","6","7","8","9","10", "Jack", "Queen", "Ace","King"
+            }; 
+
+        }
+    }
+}
